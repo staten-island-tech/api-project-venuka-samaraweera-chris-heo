64a4ecf895f05a75b00fded07b04a55672a2798a
heochristopher <christopher.heo1223@gmail.com>
2021-05-12 10:07:37 -0400

    display random cities

--- src/js/index.js
+++ src/js/index.js
@@ -1,38 +1,40 @@
-const DOMSelectors = document.querySelector(".container");
+const DOMSelectors = document.querySelector(".flex-container");
 
-const query = async function () {
+function randomCoords(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+async function query() {
   try {
+    const lat = randomCoords(-90, 90);
+    const lon = randomCoords(-180, 180);
     const response = await fetch(
-      "https://api.sunrise-sunset.org/json?lat=40.730610&lng=-73.935242&date=today"
+      `https://api.sunrise-sunset.org/json?lat=${lat}&lng=${lon}&date=today`
     );
     const data = await response.json();
     DOMSelectors.insertAdjacentHTML(
       "beforeend",
-      `<div class="flex-container">
-        <div class="card-container">
-            <h2 class="card-header">Sunrises right now</h2>
-            <div class="card">
-             <h3 class="city-name">New York</h3>
-             <h3 class="time">sunrise: ${data.results.sunrise}</h3>
-              <h3 class="time">sunset: ${data.results.sunset}</h3>
-              <p class="view-time">View at ${data.results.sunset} - ${data.results.nautical_twilight_end} for optimal sunset view</p>
-            </div>
-        </div>
-        <div class="card-container">
-            <h2 class="card-header">Sunsets right now</h2>
-            <div class="card">
-             <h3 class="city-name">sumwhere else</h3>
-             <h3 class="time">sunrise: ${data.results.sunrise}</h3>
-              <h3 class="time">sunset: ${data.results.sunset}</h3>
-              <p class="view-time">View at ${data.results.sunset} - ${data.results.nautical_twilight_end} for optimal sunset view</p>
-            </div>
-        </div>
-       </div>`
+      `<div class="card-container">
+          <div class="card">
+            <h3 class="city-name">${`${lat}, ${lon}`}</h3>
+            <h3 class="time">sunrise: ${data.results.sunrise}</h3>
+            <h3 class="time">sunset: ${data.results.sunset}</h3>
+            <p class="view-time">View at ${data.results.sunset} - ${
+        data.results.nautical_twilight_end
+      } for optimal sunset view</p>
+          </div>
+        </div>`
     );
   } catch (error) {
     console.log(error);
     alert("oops");
   }
-};
+}
 
-query();
+i = 0;
+while (i < 3) {
+  query();
+  i++;
+}
